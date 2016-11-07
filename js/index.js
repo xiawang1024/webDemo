25e08633d6454f1f025ef1e1dbbeb684b12a93ee
unknown <>
2016-11-07 14:00:38 +0800

    完善

--- js/index.js
+++ js/index.js
@@ -10,11 +10,25 @@ $(function(){
 			$(".city-info").eq(index).addClass("cur").siblings().removeClass("cur");
 		})
 	});
-	 var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        autoplay : 3000,
-        autoplayDisableOnInteraction : false,
-        loop : true
-    });
+	var swiper = new Swiper('.swiper-container', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		autoplay : 3000,
+		autoplayDisableOnInteraction : false,
+		loop : true
+	});
+	var swiper1 = new Swiper('.swiper-container1', {
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+		spaceBetween : 6,
+		autoplay : 3000,
+		autoplayDisableOnInteraction : false,
+		loop : true
+	});
+	var oWidth=$(document).width();
+	var h=Math.ceil(oWidth/4.5);
+	$('.swiper-container1 .swiper-slide').css("height",h);
+	$(window).resize(function(){
+		console.log($(window).width()+","+$(document).width())
+	})
 })
