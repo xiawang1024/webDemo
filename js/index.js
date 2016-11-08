5a4ccaf3983318a3c00d64e304dc5d8dbe25fe17
unknown <>
2016-11-08 17:58:18 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,34 +1,47 @@
-$(function(){
+$(function() {
+	//container 高度自适应
+	(function(){
+		var oWidth = $(document).width();
+		var h = Math.ceil(oWidth / 4.5);
+		if(h>315){h=315}
+		$('.swiper-container1 .swiper-slide').css("height", h);
+		$(window).resize(function() {
+			console.log($(window).width() + "," + $(document).width())
+		})
+	})();
 	//citys动画
-	$(".citys a").each(function(index,item){
-		$(this).mouseover(function(){
-			var oA=$(this);
+	$(".citys a").each(function(index, item) {
+		$(this).mouseover(function() {
+			var oA = $(this);
 			oA.children("div").addClass("cur");
 			oA.siblings("a").children("div").removeClass("cur");
-			oA.children("span").css("color","#fff");
-			oA.siblings("a").children("span").css("color","#000");
+			oA.children("span").css("color", "#fff");
+			oA.siblings("a").children("span").css("color", "#000");
 			$(".city-info").eq(index).addClass("cur").siblings().removeClass("cur");
 		})
 	});
 	var swiper = new Swiper('.swiper-container', {
 		pagination: '.swiper-pagination',
 		paginationClickable: true,
-		autoplay : 3000,
-		autoplayDisableOnInteraction : false,
-		loop : true
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		loop: true
 	});
 	var swiper1 = new Swiper('.swiper-container1', {
-		slidesPerView : 3,
-		slidesPerGroup : 3,
-		spaceBetween : 6,
-		autoplay : 3000,
-		autoplayDisableOnInteraction : false,
-		loop : true
+		slidesPerView: 3,
+		slidesPerGroup: 3,
+		spaceBetween: 6,
+		autoplay: 3000,
+		autoplayDisableOnInteraction: false,
+		loop: true
 	});
-	var oWidth=$(document).width();
-	var h=Math.ceil(oWidth/4.5);
-	$('.swiper-container1 .swiper-slide').css("height",h);
-	$(window).resize(function(){
-		console.log($(window).width()+","+$(document).width())
+	
+	$(".body-height .list").each(function(index,item){
+			var _this=$(this);
+		_this.hover(function(){
+			_this.find("img").addClass("animated swing");
+		},function(){
+			_this.find("img").removeClass("animated swing");
+		})
 	})
 })
